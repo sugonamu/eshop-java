1f6abb16c45c193522679587224693eb80b3319d
sugonamu <je0jwff@gmail.com>
2025-02-14 05:51:32 +0700

    repository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Product {
+    private int ProductQuantity;
+    private String ProductName;
+    private String ProductId;
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@Repository
+public class ProductRepository {
+    private List<Product> productData= new ArrayList<>();
+
+
+    public Product create(Product product){
+        productData.add(product);
+        return product;
+    }
+
+
+
+    public Iterator<Product> findAll(){
+        return productData.iterator();
+    }
+}
