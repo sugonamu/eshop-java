2c26d5e2a403e0f39155486580edda2de2e56661
sugonamu <je0jwff@gmail.com>
2025-02-14 09:27:05 +0700

    build gradle debug

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,7 @@ dependencies {
     testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
     testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitjupiterVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitjupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitjupiterVersion")
 }
 
 tasks.register<Test>( "unitTest") {
