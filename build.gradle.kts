f684891dfae4de6627b51e3043562c9b59e34171
sugonamu <je0jwff@gmail.com>
2025-02-14 07:13:58 +0700

    Build Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id 'java'
-    id 'org.springframework.boot' version '3.4.2'
-    id 'io.spring.dependency-management' version '1.1.7'
+    java
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = 'id.ac.ui.cs.advprog'
-version = '0.0.1-SNAPSHOT'
+group = "id.ac.ui.cs.advprog"
+version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
@@ -15,31 +15,35 @@ java {
 
 configurations {
     compileOnly {
-        extendsFrom annotationProcessor
+        extendsFrom(configurations.annotationProcessor.get())
     }
 }
 
 repositories {
     mavenCentral()
 }
+
 val junitjupiterVersion ="5.9.1"
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
 
 dependencies {
-    implementation 'org.springframework.boot:spring-boot-starter-web'
-    implementation 'org.springframework.boot:spring-boot-starter-thymeleaf'  // Add this line
-    compileOnly 'org.projectlombok:lombok'
-    developmentOnly 'org.springframework.boot:spring-boot-devtools'
-    annotationProcessor 'org.springframework.boot:spring-boot-configuration-processor'
-    annotationProcessor 'org.projectlombok:lombok'
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.seleniumhq.selenium:selenium-java:4.14.1")
-    testImplementation("io.github.bonigarcia:selenium-jupiter:5.0.1")
-    testImplementation("io.github.bonigarcia:webdrivermanager:5.6.3")
-    testImplementation("org.junit.jupiter:junit-jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitjupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitjupiterVersion")
 }
+
 tasks.register<Test>( "unitTest") {
     description = "Runs unit tests."
     group = "verification"
@@ -60,7 +64,6 @@ tasks.register<Test>( "functionalTest") {
 
 }
 
-
-tasks.withType<Test>().configureEach {
+tasks.withType<Test>().configureEach() {
     useJUnitPlatform()
 }
