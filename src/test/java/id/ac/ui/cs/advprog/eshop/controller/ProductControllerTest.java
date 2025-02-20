0ac59b7074399403f1c9dd12aa20a578c8014333
sugonamu <je0jwff@gmail.com>
2025-02-20 23:30:58 +0700

    fix ci

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import java.util.Collections;
 import java.util.UUID;
@@ -19,8 +18,7 @@ public class ProductControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
+    
     private ProductService productService;
 
     @Test
--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -22,7 +20,6 @@ public class ProductServiceImplTest {
     @Autowired
     private ProductServiceImpl productService;
 
-    @MockBean
     private ProductRepository productRepository;
 
     private Product product;
